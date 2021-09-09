f78e61ecfc899f0d3a4cb613b621d10130472da2
thayklain <thay.klain@gmail.com>
2021-09-09 08:11:00 -0300

    first commit

---
+++ src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,23 @@
+package com.dio.live.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class JornadaTrabalho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+}
